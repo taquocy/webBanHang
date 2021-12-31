e946e394ac60021836c68ad4a7853348eb253a9d
taquocy <taquocitali@gmail.com>
2021-12-31 14:34:13 +0700

    init project

---
+++ LDP_MyPham_Innisfree-Forest/js/app.js
@@ -0,0 +1,164 @@
+﻿(function (comdy, $) {
+    var settings = {};
+    comdy.app = comdy.app || {};
+
+    comdy.app.init = function (options) {
+        comdy.app.setOptions(options);
+		$('[data-lightbox="inline"]').magnificPopup({
+			type: 'inline',
+			mainClass: 'mfp-no-margins mfp-fade',
+			closeBtnInside: false,
+			fixedContentPos: true
+		});
+    };
+
+    comdy.app.setOptions = function (options) {
+        settings = $.extend(settings, options);
+    };
+
+    comdy.app.subscribe = function (formId) {
+        var errorMessage = '';
+        if ($(formId + " #Email").val() === '') {
+            errorMessage += "Bạn chưa nhập Email";
+        }
+
+        if (errorMessage === '') {
+            $.ajax({
+                type: 'POST',
+                url: '/api/comdy/' + $('#token').val() + '/subscribe',
+                data: JSON.stringify({ "Email": $(formId + " #Email").val() }),
+                dataType: 'json',
+                contentType: 'application/json',
+                success: function (response) {
+                    if (response.success) {
+                        new PNotify({
+                            text: "Đăng ký nhận tin tức về sự kiện thành công.",
+                            type: "success",
+                            buttons: {
+                                sticker: false,
+                                closer: false
+                            }
+                        });
+                        $(formId + " #Email").val('');
+
+                        var magnificPopup = $.magnificPopup.instance;
+                        // save instance in magnificPopup variable
+                        magnificPopup.close();
+                    } else {
+                        new PNotify({
+                            text: response.message,
+                            type: "error",
+                            buttons: {
+                                sticker: false,
+                                closer: false
+                            }
+                        });
+                    }
+                }
+            });
+        } else {
+            new PNotify({
+                text: errorMessage,
+                type: "error",
+                buttons: {
+                    sticker: false,
+                    closer: false
+                }
+            });
+        }
+    };
+
+    comdy.app.order = function () {
+        var errorMessage = '';
+        if ($("#modal-order #FullName").val() === '') {
+            errorMessage += "Chưa nhập Họ Tên";
+        }
+        if ($("#modal-order #PhoneNumber").val() === '') {
+            if (errorMessage !== '') {
+                errorMessage += "<br/>Chưa nhập Điện thoại";
+            }
+            else {
+                errorMessage += "Chưa nhập Điện thoại";
+            }
+        }
+        if ($("#modal-order #Email").val() === '') {
+            if (errorMessage !== '') {
+                errorMessage += "<br/>Chưa nhập Email";
+            }
+            else {
+                errorMessage += "Chưa nhập Email";
+            }
+        }
+
+        if (errorMessage === '') {
+            var product = $("#modal-order-form #Product").val();
+            var quantity = $("#modal-order-form #Quantity").val();
+            var data = {
+                "FullName": $("#modal-order-form #FullName").val(),
+                "Email": $("#modal-order-form #Email").val(),
+                "PhoneNumber": $("#modal-order-form #PhoneNumber").val(),
+                "OrderDetail": "Kem Nền Trang Điểm Dành Cho Nam Innisfree Forest For Men Grooming BB Cream SPF50+PA++++ - " + + " - Số lượng: " + quantity + " hộp",
+                "TotalAmount": parseInt(quantity) * 490000
+            };
+            $.ajax({
+                type: 'POST',
+                url: '/api/comdy/' + $('#token').val() + '/order',
+                data: JSON.stringify(data),
+                dataType: 'json',
+                contentType: 'application/json',
+                success: function (response) {
+                    if (response.success) {
+                        new PNotify({
+                            text: "Đặt mua thành công.",
+                            type: "success",
+                            buttons: {
+                                sticker: false,
+                                closer: false
+                            }
+                        });
+                        $("#modal-order-form #FullName").val('');
+                        $("#modal-order-form #PhoneNumber").val('');
+                        $("#modal-order-form #Email").val('');
+
+                        var magnificPopup = $.magnificPopup.instance;
+                        // save instance in magnificPopup variable
+                        magnificPopup.close();
+                    } else {
+                        new PNotify({
+                            text: response.message,
+                            type: "error",
+                            buttons: {
+                                sticker: false,
+                                closer: false
+                            }
+                        });
+                    }
+                }
+            });
+        } else {
+            new PNotify({
+                text: errorMessage,
+                type: "error",
+                buttons: {
+                    sticker: false,
+                    closer: false
+                }
+            });
+        }
+    };
+
+    function getProductName(product) {
+        switch(product) {
+            case 1:
+                return "Màu 1 Bright Skin";
+            case 2:
+                return "Màu 2 Normal Skin";
+            default:
+                return "";
+        }
+    }
+}(window.comdy = window.comdy || {}, jQuery));
+
+$(function(){
+    comdy.app.init({});
+});
